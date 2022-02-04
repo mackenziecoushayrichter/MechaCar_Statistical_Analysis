d84cdab9df932e90d6ea0f55ec2a3d8f158a8209
mackenziecoushay <mackenziecoushay@Mackenzies-MacBook-Air.local>
2022-02-04 11:19:15 -0800

    saving changes

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,19 @@
-
+#Deliverable 1
 library(dplyr)
 install.packages("dplyr")
 library('tidyverse')
 mechaCar_mpg <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F) #import and read csv into dataframe
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechaCar_mpg)#perform linear regression
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechaCar_mpg)) #use summary function to find p-value and r-squared value
+#Deliverable 2
+suspension_coil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F) #import and read csv into dataframe
+total_summary <- suspension_coil%>% summarize(mean=mean(PSI),
+                                              median=median(PSI),
+                                              var=var(PSI),
+                                              sd=sd(PSI),
+                                              .groups = 'keep')
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI),
+                                                                             median=median(PSI),
+                                                                             var=var(PSI),
+                                                                             sd=sd(PSI),
+                                                                             .groups = 'keep')
